fd7ad736ad545869089b6e75a2b7b679a1a28769
Kostas Xanthopoulos <konxantho@gmail.com>
2021-12-10 11:41:54 +0200

    refactoring

--- 2021/python/day09.py
+++ 2021/python/day09.py
@@ -1,67 +1,59 @@
 from math import prod
-from typing import List
+from typing import Generator, Dict, Tuple
 
 from utils import get_input_text
 
 
-def get_input_list() -> List[List[int]]:
+def get_input_grid() -> Dict[Tuple[int, int], int]:
     input_text = get_input_text(9)
-    return [[int(x) for x in y] for y in input_text.split('\n')]
+    grid = {}
+    for y, line in enumerate(input_text.split('\n')):
+        for x, c in enumerate(line):
+            grid[(y, x)] = int(c)
+    return grid
+
+
+def get_adjacent(y: int, x: int) -> Generator[Tuple[int, int], None, None]:
+    yield y - 1, x
+    yield y + 1, x
+    yield y, x - 1
+    yield y, x + 1
 
 
 def get_sum_of_risk() -> int:
     total_risk = 0
-    width, height = len(heightmap[0]), len(heightmap)
-
-    for y in range(height):
-        for x in range(width):
-            neighbors = [
-                heightmap[y - dy][x - dx]
-                for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]
-                if 0 <= y - dy < height and 0 <= x - dx < width
-            ]
-            if (el := heightmap[y][x]) < min(neighbors):
-                total_risk += el + 1
+    for (y, x), c in heightmap.items():
+        if all(heightmap.get(pt, 9) > c for pt in get_adjacent(y, x)):
+            total_risk += c + 1
 
     return total_risk
 
 
 def get_basins_length() -> int:
     basin_lenghts = []
-    width, height = len(heightmap[0]), len(heightmap)
     seen = set()  # (y, x)
 
     def _get_basin_length(y: int, x: int):
-        if (y, x) in seen or heightmap[y][x] == 9:
+        if (y, x) in seen or heightmap.get((y, x), 9) == 9:
             return 0
 
         # current location
         seen.add((y, x))
         basin_legth = 1
 
-        # right
-        if x < len(heightmap[0]) - 1:
-            basin_legth += _get_basin_length(y, x + 1)
-        # down
-        if y < len(heightmap) - 1:
-            basin_legth += _get_basin_length(y + 1, x)
-        # left
-        if x:
-            basin_legth += _get_basin_length(y, x - 1)
-        # up
-        if y:
-            basin_legth += _get_basin_length(y - 1, x)
+        # right, down, left, up
+        basin_legth += _get_basin_length(y, x + 1)
+        basin_legth += _get_basin_length(y + 1, x)
+        basin_legth += _get_basin_length(y, x - 1)
+        basin_legth += _get_basin_length(y - 1, x)
 
         return basin_legth
 
-    basin_lenghts = [
-        _get_basin_length(y, x) for y in range(height) for x in range(width)
-    ]
-
+    basin_lenghts = [_get_basin_length(pt[0], pt[1]) for pt in heightmap.keys()]
     return prod(sorted(basin_lenghts, reverse=True)[:3])
 
 
-heightmap = get_input_list()
+heightmap = get_input_grid()
 
 risk = get_sum_of_risk()
 print(f'Risk: {risk}')
