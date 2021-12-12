33ab412165e6b911a04a27d5e33fa9d02a98044a
Kostas Xanthopoulos <konxantho@gmail.com>
2021-12-12 02:57:04 +0200

    put implementation inside of functions

--- 2021/python/template.py
+++ 2021/python/template.py
@@ -1,56 +1,44 @@
-import pytest
+from typing import List, Dict, Tuple
 
-from utils import get_input_text, get_example_input_text
+from utils import get_input_text
 
-EXPECTED_1 = None
-EXPECTED_2 = None
+EXAMPLE_INPUT = '''\
+'''
+# EXPECTED_1 =
+# EXPECTED_2 =
 
 
-def get_input_data(example=False):
+def get_input_data(example=False) -> None:
     if example:
-        input_text = get_example_input_text()
+        input_text = EXAMPLE_INPUT
     else:
-        input_text = get_input_text(10)
+        input_text = get_input_text(11)
     # TODO: implement code here
-    return input_text
-
-
-lines = get_input_data()
-
-
-def solve_part_1():
     return
 
 
-def solve_part_2():
+input_data = get_input_data(False)
+
+
+def solve_1() -> int:
+    # TODO: implement solution here
     pass
 
 
-@pytest.mark.parametrize(
-    ('input', 'expected'),
-    ((get_input_data(example=True), EXPECTED_1), ),
-)
-def test_part_1(input: str, expected: int) -> None:
-    assert solve_part_1(input) == expected
+def solve_2() -> int:
+    # TODO: implement solution here
+    pass
 
 
-@pytest.mark.parametrize(
-    ('input', 'expected'),
-    ((get_input_data(example=True), EXPECTED_2), ),
-)
-def test_part_2(input: str, expected: int) -> None:
-    assert solve_part_2(input) == expected
-
-
-def main():
+def solve():
     # part 1
-    part_1 = solve_part_1(lines)
+    part_1 = solve_1()
     print(f'Part 1: {part_1}')
 
     # part 2
-    part2 = solve_part_2(lines)
-    print(f'Part 2: {part2}')
+    part_2 = solve_2()
+    print(f'Part 1: {part_2}')
 
 
 if __name__ == '__main__':
-    main()
+    solve()
