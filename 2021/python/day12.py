3d981e088e4741fd437954f37a9f1253fdd35f9b
Xanthopoulos Konstantinos <konxantho@gmail.com>
2021-12-20 01:00:09 +0200

    python day 12 part 2

--- 2021/python/day12.py
+++ 2021/python/day12.py
@@ -1,4 +1,5 @@
-from typing import List, Dict, Tuple
+from os import path
+from typing import List, Dict
 from collections import defaultdict
 
 from utils import get_input_text
@@ -13,7 +14,7 @@ A-end
 b-end
 '''
 # EXPECTED_1 = 10
-# EXPECTED_2 = 103
+# EXPECTED_2 = 36
 
 
 def get_input_data(example=False) -> defaultdict:
@@ -23,21 +24,21 @@ def get_input_data(example=False) -> defaultdict:
         input_text = get_input_text(12)
 
     lines = input_text.strip().splitlines()
-    connections = defaultdict(list)
+    connections = defaultdict(set)
     for line in lines:
         p1, p2 = line.split('-')
-        connections[p1].append(p2)
-        connections[p2].append(p1)
+        connections[p1].add(p2)
+        connections[p2].add(p1)
     return connections
 
 
-connections = get_input_data(True)
+connections = get_input_data()
 
 
 def solve_1(cave: str, visited=set()) -> int:
     if cave == 'end':
         return 1
-    
+
     if cave.islower() and cave != 'end':
         visited.add(cave)
     connected_caves = [x for x in connections[cave] if x not in visited]
@@ -50,16 +51,29 @@ def solve_1(cave: str, visited=set()) -> int:
 
 
 def solve_2() -> int:
-    # TODO: implement solution here
-    pass
+    total_paths = set()
+    todo = [(('start', ), False)]
+    while todo:
+        path, double_cave = todo.pop()
+
+        if path[-1] == 'end':
+            total_paths.add(path)
+            continue
+
+        for cand in connections[path[-1]]:
+            if cand == 'start':
+                continue
+            elif cand.isupper() or cand not in path:
+                todo.append(((*path, cand), double_cave))
+            elif not double_cave and path.count(cand) == 1:
+                todo.append(((*path, cand), True))
+    return len(total_paths)
 
 
 def solve():
-    # part 1
-    part_1 = solve_1('start', set(['start']))
+    part_1 = solve_1('start')
     print(f'Part 1: {part_1}')
 
-    # part 2
     part_2 = solve_2()
     print(f'Part 2: {part_2}')
 
