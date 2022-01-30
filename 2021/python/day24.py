0c2209ada0695d87a6e43e99a9673591eb3ae6d9
xanthoko <konxantho@gmail.com>
2022-01-30 18:52:23 +0200

    Add example input

--- 2021/python/day24.py
+++ 2021/python/day24.py
@@ -3,6 +3,17 @@ from typing import List, Dict, Tuple, Optional
 from utils import get_input_text
 
 EXAMPLE_INPUT = '''\
+inp w
+add z w
+mod z 2
+div w 2
+add y w
+mod y 2
+div w 2
+add x w
+mod x 2
+div w 2
+mod w 2
 '''
 # EXPECTED_1 =
 # EXPECTED_2 =
