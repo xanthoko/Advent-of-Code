17696f564fb96c6e3b4ee2b89846741c3a46f04b
xanthoko <konxantho@gmail.com>
2022-02-27 12:18:05 +0200

    day 3

--- 2019/day03.py
+++ 2019/day03.py
@@ -1,15 +1,20 @@
-from typing import Tuple, Set, List
+from __future__ import annotations
 
-from utils import get_input_text, get_example_input_text
+import sys
+
+from utils import get_input_text
 
 input_text = get_input_text(3)
-input_text = get_example_input_text()
 paths = list(map(lambda x: x.split(','), input_text.split('\n')))
+print()
 
 
-def create_trace(path: List[str]) -> Set[Tuple[int, int]]:
+def create_trace(
+        path: list[str]) -> tuple[set[tuple[int, int]], dict[tuple[int, int], int]]:
     DIRECTION_MAP = {'R': (1, 0), 'L': (-1, 0), 'U': (0, -1), 'D': (0, 1)}
     trace = set()
+    steps_map: dict[tuple[int, int], int] = {}
+    steps = 0
     pos = (0, 0)
     for instruction in path:
         direction = instruction[0]
@@ -18,16 +23,28 @@ def create_trace(path: List[str]) -> Set[Tuple[int, int]]:
         for _ in range(distance):
             pos = (pos[0] + dx, pos[1] + dy)
             trace.add(pos)
-    return trace
+            steps += 1
+            if pos not in steps_map:
+                steps_map[pos] = steps
+    return trace, steps_map
 
 
-def _get_manhatan(pos: Tuple[int, int]) -> int:
+def _get_manhatan(pos: tuple[int, int]) -> int:
     return abs(pos[0]) + abs(pos[1])
 
 
-trace_1 = create_trace(paths[0])
-trace_2 = create_trace(paths[1])
+trace_1, steps_map_1 = create_trace(paths[0])
+trace_2, steps_map_2 = create_trace(paths[1])
 interesection_points = trace_1 & trace_2
 
 man_distances = [_get_manhatan(x) for x in interesection_points]
-print(f'[PART 1] The closes distance is {min(man_distances)}')
+print(f'[PART 1] The closest distance is {min(man_distances)}')
+
+min_steps = sys.maxsize
+for intersection_point in interesection_points:
+    wire_1_steps = steps_map_1[intersection_point]
+    wire_2_steps = steps_map_2[intersection_point]
+    if (sum_steps := wire_1_steps + wire_2_steps) < min_steps:
+        min_steps = sum_steps
+
+print(f'[PART 2] The smallest number of steps is: {min_steps}')
